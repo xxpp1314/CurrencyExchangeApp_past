a0cfb1407da48b2f4f976d74a92a13a08673957a
xxpp1314 <317211206@qq.com>
2021-10-21 19:56:52 -0700

    Initial Commit

---
+++ CurrencyExchangeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyExchangeApp
+//
+//  Created by Yinxing Gao on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
